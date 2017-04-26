72f4d30ca7cc7dea719a8026868f8fe4737e12c2
Wojciech Druzkawiecki <exerq4@gmail.com>
2017-04-26 15:54:32 +0200

    Multi filter for places

---
+++ assets/js/custom.js
@@ -0,0 +1,25 @@
+// store filter for each group
+var filters = {};
+
+$demo = $(".masonry")
+$grid = $(".masonry__container")
+
+$demo.on( 'click', '.input-select', function() {
+  var $this = $(this);
+  // get group key
+  var filterGroup = $this.attr('data-filter-group');
+  // set filter for group
+  filters[ filterGroup ] = $this[0].children[0].value;
+  // combine filters
+  var filterValue = concatValues( filters );
+  $grid.isotope({ filter: filterValue });
+});
+
+// flatten object by concatting values
+function concatValues( obj ) {
+  var value = '';
+  for ( var prop in obj ) {
+    value += obj[ prop ];
+  }
+  return value;
+}
